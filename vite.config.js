df03806bac3e60f95f7036bc15afbb1cfd041b59
GYH-08042 <2916838993@qq.com>
2023-03-18 21:22:29 +0800

    correct path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'//手动导入elementplus
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     ElementPlus()
