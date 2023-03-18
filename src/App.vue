84826140eac6003520a74f9239ad4307179027c4
GYH-08042 <2916838993@qq.com>
2023-03-18 17:09:39 +0800

    initial version

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CheckToDo from './components/CheckToDo.vue'
+</script>
+
+<template>
+  <CheckToDo />
+</template>
+
+<style scoped>
+body {
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+  padding: 20px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'//手动导入elementplus
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    ElementPlus()
+  ],
+  server: {
+    host: '192.168.1.3',//设置IP
+    port: 9999,//设置端口
+    strictPort: true,//若端口被占用不自动寻找下一个可用端口
+  }
+})
